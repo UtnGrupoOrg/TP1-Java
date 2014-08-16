8f0ce74cc35302fae31de5b8cced503461f2380a
Simonpedro <pedritos506@hotmail.com>
2014-08-16 12:50:27 -0300

    Correccion otra vez de cambios que en anterior push no se habia modificado.

--- Business.Logic/src/logicaNegocio/ElectrodomesticoLogic.java
+++ Business.Logic/src/logicaNegocio/ElectrodomesticoLogic.java
@@ -1,24 +1,15 @@
 package logicaNegocio;
 
-import data.PrecioPorPeso;
-import data.PreciosPorConsumoEnergetico;
 
-import entidades.Electrodomestico;
+
 
 public class ElectrodomesticoLogic {
 	
-	private PreciosPorConsumoEnergetico oPreciosConsumo;
-	private PrecioPorPeso oPreciosPeso;
+	
 	
 	public ElectrodomesticoLogic() {
-		this.oPreciosConsumo = new PreciosPorConsumoEnergetico();
-		this.oPreciosPeso = new PrecioPorPeso();
+		
 	}
 	
-	public double calculaPrecioFinal(Electrodomestico electrodomestico) {
-		double precio= this.oPreciosConsumo.getPrecio(electrodomestico.getConsumoEnergetico())
-				+ this.oPreciosPeso.getPrecio(electrodomestico.getPeso());
-		
-		return precio;
-	}
+
 }
--- Data/src/data/PreciosPorConsumoEnergetico.java
+++ Data/src/data/PreciosPorConsumoEnergetico.java
@@ -1,35 +1,27 @@
 package data;
 
 import java.util.ArrayList;
+
 import entidades.*;
 
 public class PreciosPorConsumoEnergetico {
 	
 	
 
-	private static  ArrayList<ConsumoEnergetico_Precio> listaPrecios = inicializaLista();
+	private static  ArrayList<ConsumoEnergetico> listaPrecios = inicializaLista();
 
-	private static ArrayList<ConsumoEnergetico_Precio> inicializaLista() {
-		ArrayList<ConsumoEnergetico_Precio> lista = new ArrayList<ConsumoEnergetico_Precio>();
-		lista.add(new ConsumoEnergetico_Precio('A', 100));
-		lista.add(new ConsumoEnergetico_Precio('B', 80));
-		lista.add(new ConsumoEnergetico_Precio('C', 60));
-		lista.add(new ConsumoEnergetico_Precio('D', 50));
-		lista.add(new ConsumoEnergetico_Precio('E', 30));
-		lista.add(new ConsumoEnergetico_Precio('F', 10));
+	private static ArrayList<ConsumoEnergetico> inicializaLista() {
+		ArrayList<ConsumoEnergetico> lista = new ArrayList<ConsumoEnergetico>();
+		lista.add(new ConsumoEnergetico('A', 100));
+		lista.add(new ConsumoEnergetico('B', 80));
+		lista.add(new ConsumoEnergetico('C', 60));
+		lista.add(new ConsumoEnergetico('D', 50));
+		lista.add(new ConsumoEnergetico('E', 30));
+		lista.add(new ConsumoEnergetico('F', 10));
 		
 		return lista;
 	}
 
-	public double getPrecio(char consumoEnergetico) {
-		double precio=0;
-		for (ConsumoEnergetico_Precio consumoEnergetico_Precio : listaPrecios) {
-			if (consumoEnergetico_Precio.getConsumo()==consumoEnergetico) {
-				precio = consumoEnergetico_Precio.getPrecio();
-			}
-		}
-		return precio;
-	}
 	
 	
 	
